cf6b602d506e4bdddb5a6f14383cc7cfa3fe36da
FofN <fofn@users.github.com>
2021-02-17 16:26:12 -0500

    Created Navbar
    + Menu and menu items have been added

---
+++ proj1-team5.php
@@ -0,0 +1,65 @@
+<!--
+CPS630 - Project Iteration 1
+Section 2 Members:
+	Justin Maliwat	500899430
+	Diwei Guan		500879852
+	Deep Oza		500830262
+-->
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Lab 2 Project 1</title>
+        
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="http://localhost/CPS630/Project1/proj1-team5.css"/>
+    </head>
+
+    <body>
+
+        <nav class="navbar navbar-expand navbar-dark bg-dark">
+            <div class="container-fluid">
+
+                <!-- Title of NavBar -->
+                <a class="navbar-brand" href="#">
+                    Plan for Smart Devices
+                </a>
+
+                <!-- Navbar options left -->
+                <div class="navbar-nav mr-auto">
+
+                    <!-- Links -->
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">About Us</a>
+                    <a class="nav-link" href="#">Contact Us</a>
+                    <a class="nav-link" href="#">Reviews</a>
+
+                    <!-- Serivces -->
+                    <div class="dropdown">
+                        <a class="nav-link dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Types of Services
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Buber Drive</a>
+                            <a class="dropdown-item" href="#">Buber Eats</a>
+                        </div>
+                    </div>
+                    
+                </div>
+
+                <!-- Navbar options right -->
+                <div class="navbar-nav ml-auto">
+                    <a class="nav-link" href="#">Shopping Cart</a>
+                    <a class="nav-link" href="#">Sign Up</a>
+                </div>
+            </div>
+        </nav>
+
+    </body>
+
+    <!-- Optional JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</html>
